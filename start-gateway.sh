387bea4d42e2fdc19716b7e6503841bc6a365dfe
Leslie Huang <leslie.huang@basf.com>
2026-07-02 14:08:27 +0800

    hello

--- start-gateway.sh
+++ start-gateway.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
-PEARLD_RPC_URL="http://100.83.112.42:44107" \
+PEARLD_RPC_URL="http://100.93.219.53:44107" \
 PEARLD_RPC_USER="rpcuser" \
 PEARLD_RPC_PASSWORD="rpcpass" \
 PEARLD_MINING_ADDRESS="prl1pf2uu40cgjrqs0000rgv592fscn65patpgel6yzzlaak6rearpxqqjpegjc" \
 LOGGING_LEVEL="debug" \
-uv run pearl-gateway start 
+uv run --package pearl-gateway pearl-gateway start 
 
